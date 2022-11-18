2d6f0b0ea4c162d767b87ac2a532bc8c624e3350
AlexanderMeynik <alartwins2001@gmail.com>
2022-11-18 14:55:11 +0300

    Java fx configuration, setting up directories, add first form with some controller features

---
+++ client/src/main/java/ru/edu/spbstu/client/ClientApplication.java
@@ -0,0 +1,38 @@
+package ru.edu.spbstu.client;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClientApplication extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        URL fxmlUrl = this.getClass().getClassLoader()
+                .getResource("fmls/login_register.fxml");
+        if(fxmlUrl==null)
+        {
+            System.out.println("Not found");
+            return;
+        }
+        //launch scene
+        Parent root = FXMLLoader.load(fxmlUrl);
+        primaryStage.setTitle("Running button demo");
+        primaryStage.setScene(new Scene(root, 600, 450));
+        primaryStage.show();
+
+    }
+}
+
+
+
+
+

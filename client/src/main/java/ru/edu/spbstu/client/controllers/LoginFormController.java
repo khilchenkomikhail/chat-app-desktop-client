4b092a04e9df96bfbb346ef72718231573bdac42
AlexanderMeynik <alartwins2001@gmail.com>
2022-11-25 16:02:13 +0300

    Add clientComponentsPackage, add new dependencies, implement PasswordTextField class, configure button states on form 1.

--- client/src/main/java/ru/edu/spbstu/client/controllers/LoginFormController.java
+++ client/src/main/java/ru/edu/spbstu/client/controllers/LoginFormController.java
@@ -1,72 +1,83 @@
 package ru.edu.spbstu.client.controllers;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Bounds;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ContextMenu;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-
-import java.util.concurrent.ThreadLocalRandom;
+import ru.edu.spbstu.clientComponents.PasswordTextField;
 
 public class LoginFormController {
-    public ContextMenu ar;
     public CheckBox s;
     public Button forgetPasswordButton;
+    public TextField loginTextBox;
+    public PasswordTextField passwordTextBox;
+    public TextField emailTextBox;
+    public TextField regLoginTextBox;
+    public PasswordTextField regPasswordTextBox;
+    public Button logInButton;
+    public Button registerButton;
+
     ContextMenu contextMenu = new ContextMenu();
 
     @FXML
-    void leftMouseClick()
-    {
-
-        System.out.println("Clicl");
-        //boolean state=s.isSelected();
-        //Change check box value
-        s.setSelected(!s.isSelected());
-        //Get screen bounds
-        Bounds boundsInScreen = s.localToScreen(s.getBoundsInLocal());
-
-        //show context menu() at th center of th screen
-        contextMenu.show(s,boundsInScreen.getCenterX(),boundsInScreen.getCenterY());
-    }
-
-
-    public void leftMouseDrageEnter(MouseEvent mouseEvent) {
-
-        double lx=305.0, ly=172.0;
-        //some obscure calculations to move button in random spot on screen
-        forgetPasswordButton.setTranslateX(ThreadLocalRandom.current().nextDouble(-lx+50, 550-lx));
-        forgetPasswordButton.setTranslateY(ThreadLocalRandom.current().nextDouble(-ly+50, 400-ly));
-        double x=forgetPasswordButton.getTranslateX(),y=forgetPasswordButton.getTranslateY();
-    }
-    @FXML
     void initialize() {
-        // create menuitems
-        javafx.scene.control.MenuItem menuItem1 = new javafx.scene.control.MenuItem("Some action 1");
-        javafx.scene.control.MenuItem menuItem2 = new javafx.scene.control.MenuItem("Some action 2");
-        javafx.scene.control.MenuItem menuItem3 = new javafx.scene.control.MenuItem("Some action 3");
+
+
         //setting on action callback
-        menuItem1.setOnAction(this::menuItem1Action);
+        logInButton.setDisable(true);
+        registerButton.setDisable(true);
         //You can even create callbacks using lambdas https://www.tutorialspoint.com/how-to-add-action-listeners-to-contextmenu-in-javafx
-        contextMenu.getItems().add(menuItem1);
-        contextMenu.getItems().add(menuItem2);
-        contextMenu.getItems().add(menuItem3);
-        s.setContextMenu(contextMenu);
     }
 
-    private void menuItem1Action(javafx.event.ActionEvent actionEvent) {
-        System.out.println("Some action with menuItem1");
+
+    public void logInButtonLeftMouseClick(MouseEvent mouseEvent) {
+
+        System.out.println("login "+loginTextBox.getText()+" "+passwordTextBox.getText());
     }
 
-    public void leftMouseExit(MouseEvent mouseEvent) {
-        //TODO uncomment to make mouse follow the button
-        //Scene scene = forgetPasswordButton.getScene();
-        //doublea a=new Robot().mouseMove(;
-       // Bounds boundsInScreen = forgetPasswordButton.localToScreen(forgetPasswordButton.getBoundsInLocal());
-        //GlassRobot robot = com.sun.glass.ui.Application.GetApplication().createRobot();
-        //new Robot().mouseMove((int)(boundsInScreen.getCenterX()), (int)(boundsInScreen.getCenterY()));
-        //robot.mouseMove((int)(boundsInScreen.getCenterX()), (int)(boundsInScreen.getCenterY()));
-        //forgetPasswordButton.setTranslateX(0);
-       // forgetPasswordButton.setTranslateY(0);
+    public void forgotPasswordButtonClick(MouseEvent mouseEvent) {
+        System.out.println("forgot "+loginTextBox.getText());
+    }
+
+    public void registerButtonMouseClick(MouseEvent mouseEvent) {
+        System.out.println("register "+regLoginTextBox.getText()+" "+emailTextBox.getText()+" "+regPasswordTextBox.getText());
+    }
+
+    public void updateLogin(KeyEvent keyEvent) {
+        if(loginTextBox.getText().equals(""))
+        {
+            logInButton.setDisable(true);
+            return;
+        }
+        if(passwordTextBox.getText().equals(""))
+        {
+            logInButton.setDisable(true);
+            return;
+        }
+        logInButton.setDisable(false);
+    }
+    public void updateRegisterButton(KeyEvent keyEvent)
+    {
+        if(emailTextBox.getText().equals(""))
+        {
+            registerButton.setDisable(true);
+            return;
+        }
+        if(regLoginTextBox.getText().equals(""))
+        {
+            registerButton.setDisable(true);
+            return;
+        }
+
+        if(regPasswordTextBox.getText().equals(""))
+        {
+            registerButton.setDisable(true);
+            return;
+        }
+
+        registerButton.setDisable(false);
     }
 }

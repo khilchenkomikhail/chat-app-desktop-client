f30b37b901c90ae32be612b13445696680838369
Julia <julialov2001@gmail.com>
2022-12-08 20:23:24 +0300

    Implement profile endpoints
     - Implement update_email
     - Implement get_user
     - Implement update_password
     - Implement get_profile_photo
     - Implement update_profile_photo

--- backend/src/main/java/ru/edu/spbstu/dao/UserRepository.java
+++ backend/src/main/java/ru/edu/spbstu/dao/UserRepository.java
@@ -2,7 +2,10 @@ package ru.edu.spbstu.dao;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ru.edu.spbstu.model.jpa.UserJpa;

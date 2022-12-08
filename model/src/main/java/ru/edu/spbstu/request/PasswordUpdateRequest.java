702d0c4ed959ee9dcc4f419456c270ed69d1f8df
Julia <julialov2001@gmail.com>
2022-12-08 20:23:24 +0300

    Implement profile endpoints
     - Implement update_email
     - Implement get_user
     - Implement update_password
     - Implement get_profile_photo
     - Implement update_profile_photo

---
+++ model/src/main/java/ru/edu/spbstu/request/PasswordUpdateRequest.java
@@ -0,0 +1,15 @@
+package ru.edu.spbstu.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordUpdateRequest {
+    private String login;
+    private String new_password;
+}
